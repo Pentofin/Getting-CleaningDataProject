64e7b650561ae00b3ffc3201bbbf6042dfe42fab
Pentofin <parthb95@hotmail.com>
2017-02-04 18:34:23 -0500

    First commit

---
+++ plot4.R
@@ -0,0 +1,21 @@
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data2 <- data[data$Date %in% c("1/2/2007","2/2/2007"), ]
+GAP <- as.numeric(data2$Global_active_power)
+GRP <- as.numeric(data2$Global_reactive_power)
+Volt <- as.numeric(data2$Voltage)
+dt <- strptime(paste(data2$Date, data2$Time, sep=" "), "%d/%m/%Y%H:%M:%S") 
+
+subM1 <- as.numeric(data2$Sub_metering_1)
+subM2 <- as.numeric(data2$Sub_metering_2)
+subM3 <- as.numeric(data2$Sub_metering_3)
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+plot(dt, GAP, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(dt, Volt, type="l", xlab="datetime", ylab="Voltage")
+plot(dt, subM1, type="l", ylab="Energy Submetering", xlab="")
+lines(dt, subM2, type="l", col="red")
+lines(dt, subM3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(dt, GRP, type="l", xlab="datetime", ylab="Global_reactive_power")
+dev.off()
